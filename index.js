8de4d09f4db6662bc29c29a34ec13f94fcde8032
Dennis <dennislim07@gmail.com>
2021-10-16 16:54:22 +0700

    Initial Setup

--- index.js
+++ index.js
@@ -21,6 +21,4 @@ app.use(express.static('public'))
 const Router = require('./routes/index.js')
 app.use('/', Router)
 
-app.listen(process.env.port || 3000, () => {
-    console.log("Listening to port 3000")
-})
+app.listen(process.env.port || 3000)
